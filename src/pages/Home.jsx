639bec7833a156264a0d9d7cd941ad4f18d18db6
Caroline <queenotsus@gmail.com>
2025-10-13 14:47:44 +0200

    Changed the bullet points of values

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -398,7 +398,7 @@ const Home = () => {
                       </svg>
                     </div>
                     <span className="text-white" style={{ opacity: 0.9 }}>
-                      Empowerment &amp; Growth
+                      Bold
                     </span>
                   </div>
                   
@@ -416,7 +416,7 @@ const Home = () => {
                       </svg>
                     </div>
                     <span className="text-white" style={{ opacity: 0.9 }}>
-                      Collaboration
+                      Dynamic
                     </span>
                   </div>
                   <br />
@@ -435,7 +435,7 @@ const Home = () => {
                       </svg>
                     </div>
                     <span className="text-white" style={{ opacity: 0.9 }}>
-                      Excellence Innovation
+                      Human
                     </span>
                   </div>
 
@@ -453,9 +453,46 @@ const Home = () => {
                       </svg>
                     </div>
                     <span className="text-white" style={{ opacity: 0.9 }}>
-                      Innovation
+                      Integrity
                     </span>
                   </div>
+
+                  <div className="d-flex align-items-center mb-3">
+                    <div 
+                      className="me-3 rounded-circle d-flex align-items-center justify-content-center"
+                      style={{
+                        width: '24px',
+                        height: '24px',
+                        backgroundColor: '#1e5631'
+                      }}
+                    >
+                      <svg width="12" height="12" fill="white" viewBox="0 0 16 16">
+                        <path d="M13.854 3.646a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L6.5 10.293l6.646-6.647a.5.5 0 0 1 .708 0z"/>
+                      </svg>
+                    </div>
+                    <span className="text-white" style={{ opacity: 0.9 }}>
+                      Excellence
+                    </span>
+                  </div>
+
+                  <div className="d-flex align-items-center mb-3">
+                    <div 
+                      className="me-3 rounded-circle d-flex align-items-center justify-content-center"
+                      style={{
+                        width: '24px',
+                        height: '24px',
+                        backgroundColor: '#1e5631'
+                      }}
+                    >
+                      <svg width="12" height="12" fill="white" viewBox="0 0 16 16">
+                        <path d="M13.854 3.646a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L6.5 10.293l6.646-6.647a.5.5 0 0 1 .708 0z"/>
+                      </svg>
+                    </div>
+                    <span className="text-white" style={{ opacity: 0.9 }}>
+                      Collaboration
+                    </span>
+                  </div>
+
                 </div>
               </div>
             </div>
